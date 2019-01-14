88d438d252bf3622ff592d964bb97ec7663cdec7
Raja Rao DV <rajaraodv100@gmail.com>
2019-01-14 14:41:33 -0800

    update versions

--- cypress/integration/examples/applitools.spec.js
+++ cypress/integration/examples/applitools.spec.js
@@ -6,9 +6,7 @@ describe('Hello Cypress, This is Applitools', () => {
       testName: 'My first Cypress Test',
       browser: { width: 800, height: 600 },
     });
-    cy.eyesCheckWindow('Main Page');
-    cy.get('#log-in').click();
-    cy.eyesCheckWindow('Click!');
+    cy.eyesCheckWindow('Login Page');
     cy.eyesClose();
   });
 });
