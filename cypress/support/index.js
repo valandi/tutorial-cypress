d603ce6d47755ccae44adcf9b81a74fffb96234e
Raja Rao DV <rajaraodv100@gmail.com>
2020-05-07 16:26:50 -0700

    update

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,9 +15,15 @@
 
 // Import commands.js using ES2015 syntax:
 
-import '@applitools/eyes.cypress/commands'
+import '@applitools/eyes-cypress/commands'
 
 import './commands'
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
