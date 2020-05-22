bc10acad5b50f41a7e7ee982039375af27e1d7b3
artem <tranduil15@gmail.com>
2020-05-22 10:20:00 +0300

    moving to the applitools.config.js browser config, apiKey and batch name. Set concurrency to 1.

--- cypress/integration/examples/ultraFastGrid.spec.js
+++ cypress/integration/examples/ultraFastGrid.spec.js
@@ -1,31 +1,18 @@
 describe("AppTest", () => {
 
     it(`ultraFastTest`, function () {
+        // Navigate to the url we want to test
+        // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
+        // but then change the above URL to https://demo.applitools.com/index_v2.html
+        // (for the 2nd run)
+        cy.visit('https://demo.applitools.com');
+
         // Call Open on eyes to initialize a test session
         cy.eyesOpen({
             appName: 'Demo App',
             testName: 'Ultrafast grid demo',
-            browser: [
-                // Add browsers with different viewports
-                {width: 800, height: 600, name: "chrome"},
-                {width: 700, height: 500, name: "firefox"},
-                {width: 1600, height: 1200, name: "ie11"},
-                {width: 1024, height: 768, name: "edgechromium"},
-                {width: 800, height: 600, name: "safari"},
-                // Add mobile emulation devices in Portrait mode
-                {
-                    deviceName: 'iPhone X',
-                    screenOrientation: 'portrait'
-                },
-                {
-                    deviceName: 'Pixel 2',
-                    screenOrientation: 'portrait'
-                },
-            ],
-            // set batch name to the configuration
-            batchName: 'Ultrafast Batch'
         })
-        cy.visit('https://demo.applitools.com');
+
         // check the login page with fluent api, see more info here
         // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
         cy.eyesCheckWindow({
