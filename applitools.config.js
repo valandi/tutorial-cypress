ccdc85c98dc7aa461109e95bdff1f54c5d386440
Colby Fayock <colbyfayock@users.noreply.github.com>
2021-06-25 15:09:07 -0400

    Update applitools.config.js

--- applitools.config.js
+++ applitools.config.js
@@ -14,5 +14,6 @@ module.exports = {
         {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
     ],
     // set batch name to the configuration
-    batchName: 'Demo Batch - Cypress'
+    batchName: 'Demo Batch - Cypress',
+    showLogs: true
 }
